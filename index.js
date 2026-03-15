bae1065fd7060bedc424e7cffcca636ecff1f431
Udita Singh <udita1294@gmail.com>
2026-03-16 00:36:05 +0530

    sentiment ananlysed in json format (Structured Output)

--- index.js
+++ index.js
@@ -9,11 +9,19 @@ const groq = new OpenAI({
 });
 
 const response = await groq.chat.completions.create({
+  temperature : 1,
+  // top_p: 0.2,
+  // stop : "ga",  // Negative
+  // max_completion_tokens : 500,
+  // frequency_penalty: 1,
+  // presence_penalty: 1,
+  response_format :  {type : 'json_object'},
   model: "llama-3.1-8b-instant",
   messages: [
     {
       role: "system",
-      content: "You are Jenny, a smart review grader. Your task is to review given review and return the sentiment.Classify the sentiment as positive, negative or neutral.Output must be in single word.",
+      content: `You are Jenny, a smart review grader. Your task is to review given review and return the sentiment.Classify the sentiment as positive, negative or neutral. You must give the output in JSON format with the key as sentiment and value as the sentiment of the review.
+                  example output: {"sentiment": "positive"}`,
     },
     {
       role: "user",
